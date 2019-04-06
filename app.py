043880075df576432a8721db4c79decc8109db8d
Richard Turner <rclt201@gmail.com>
2019-04-06 18:13:26 +0100

    got it into Insomnia for the first time

--- app.py
+++ app.py
@@ -11,7 +11,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+# pylint: disable=C0413
+from models.spot import Spot, SpotSchema
 
-@app.route('/')
-def home():
-    return 'Hello World', 200
+spot_schema = SpotSchema()
+
+
+#index route
+@app.route('/spots', methods=['GET'])
+def index():
+    spots = Spot.query.all()
+
+    return spot_schema.jsonify(spots, many=True), 200
+
+@app.route('/spots/<int:spot_id>', methods=['GET'])
+def show(spot_id):
+    spot = Spot.query.get(spot_id)
+    return spot_schema.jsonify(spot), 200
