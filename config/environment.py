8e2408280d8db4d1ff35c8f8cbec2096cdc23fbc
Richard Turner <rclt201@gmail.com>
2019-04-16 14:39:34 +0100

    final changes

--- config/environment.py
+++ config/environment.py
@@ -1,3 +1,4 @@
 import os
 
+db_uri = os.getenv('DATABASE_URL', 'postgres://localhost:5432/spots')
 secret = os.getenv('SECRET', 'shhh its a secret')
--- config/routes.py
+++ config/routes.py
@@ -1,8 +1,19 @@
 from app import app
 from controllers import spots, categories, auth, artists, users
+import os
+
 
 app.register_blueprint(spots.api, url_prefix='/api')
 app.register_blueprint(categories.api, url_prefix='/api')
 app.register_blueprint(auth.api, url_prefix='/api')
 app.register_blueprint(artists.api, url_prefix='/api')
 app.register_blueprint(users.api, url_prefix='/api')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+
+    if os.path.isfile('dist/' + path):
+        return app.send_static_file(path)
+
+    return app.send_static_file('index.html')
