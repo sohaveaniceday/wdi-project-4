85de49a6e443c7abdc00a4b3413c8d6530322efa
Richard Turner <rclt201@gmail.com>
2019-05-22 19:04:58 +0100

    refactoring

--- src/components/home.js
+++ src/components/home.js
@@ -24,7 +24,6 @@ class Home extends React.Component {
       params: { locationlat, locationlon, radius }
     })
       .then(res => {
-        console.log('res', res)
         this.setState({ points: res.data })
       })
   }
